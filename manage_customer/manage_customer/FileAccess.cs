eccad1ced8ae663bd3ab3abbd770af36773bcb59
truongl <lntruong797@gmail.com>
2018-02-26 08:02:04 +0700

    Root

---
+++ manage_customer/manage_customer/FileAccess.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace manage_customer
+{
+    class FileAccess
+    {
+        public void writeData(string path, string str)
+        {
+            try
+            {
+                FileStream fs = new FileStream(path, FileMode.Append);
+                StreamWriter sw = new StreamWriter(fs);
+
+                sw.WriteLine(str);
+
+                sw.Flush();
+                sw.Close();
+                sw.Dispose();
+                fs.Close();
+                fs.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not add customer!");
+            }
+        }
+
+        public string readData(string path)
+        {
+            string result = "";
+            try
+            {
+                FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+                StreamReader sr = new StreamReader(fs);
+                result = sr.ReadToEnd();
+                sr.Close();
+                sr.Dispose();
+                fs.Close();
+                fs.Dispose();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Can not get customer!");
+            }
+            return result;
+        }
+    }
+}
