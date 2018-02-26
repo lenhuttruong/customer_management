fde27391ea24c9f2da4fd6cfdcd71362233e966f
truongl <lntruong797@gmail.com>
2018-02-26 14:11:31 +0700

    Root

---
+++ manage_customer/manage_customer/DAO/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manage_customer.DAO
+{
+    class Customer
+    {
+        private string _Id;
+        private string _Name;
+        private string _Phone;
+        private string _Address;
+
+        public Customer(string Id, string Name, string Phone, string Address)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Phone = Phone;
+            this.Address = Address;
+        }
+
+        public string Id { get => _Id; set => _Id = value; }
+        public string Name { get => _Name; set => _Name = value; }
+        public string Phone { get => _Phone; set => _Phone = value; }
+        public string Address { get => _Address; set => _Address = value; }
+    }
+}
---
+++ manage_customer/manage_customer/DAO/Slot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manage_customer.DAO
+{
+    class Slot
+    {
+        private string _Id;
+        private string _Name;
+        private DateTime _Date;
+        private string _Service;
+        private string _Product;
+        private string _Staff;
+        private string _Note;
+
+        public Slot(string Id, string Name, DateTime Date, string Service, string Product, string Staff, string Note)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Date = Date;
+            this.Service = Service;
+            this.Product = Product;
+            this.Staff = Staff;
+            this.Note = Note;
+        }
+
+        public string Id { get => _Id; set => _Id = value; }
+        public string Name { get => _Name; set => _Name = value; }
+        public DateTime Date { get => _Date; set => _Date = value; }
+        public string Service { get => _Service; set => _Service = value; }
+        public string Product { get => _Product; set => _Product = value; }
+        public string Staff { get => _Staff; set => _Staff = value; }
+        public string Note { get => _Note; set => _Note = value; }
+    }
+}
